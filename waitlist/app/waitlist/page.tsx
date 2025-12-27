8f9ec779faf37fcd8ca46c8dec4a06bf16abb862
Aditya <adityas.singh.tech@gmail.com>
2025-12-27 20:39:23 +0530

    add logo in waitlist page

--- waitlist/app/waitlist/page.tsx
+++ waitlist/app/waitlist/page.tsx
@@ -4,9 +4,7 @@ import Waitlist from "@/Components/Waitlist";
 export default function WaitlistPage() {
     return (
         <main className="min-h-screen text-[#1A1A1A]">
-            <header className="fixed top-0 left-0 w-full z-50">
-                <Logo />
-            </header>
+            <Logo />
             <Waitlist />
         </main>
     );
